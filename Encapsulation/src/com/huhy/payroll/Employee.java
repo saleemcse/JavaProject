ebc86fe6882bf2d3c7cdd30df2e18ecf0201e30d
huhy <saleemcse.cs16@rvce.edu.in>
2018-07-25 11:06:24 +0530

    sss

---
+++ Encapsulation/src/com/huhy/payroll/Employee.java
@@ -0,0 +1,21 @@
+package com.huhy.payroll;
+
+public class Employee {
+
+	private int empId;
+	private String firstName;
+	private String lastName;
+	public int getEmpId() {
+		return empId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public int getDepId() {
+		return depId;
+	}
+	private int depId;
+}
